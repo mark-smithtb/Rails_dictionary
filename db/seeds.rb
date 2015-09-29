2b87268dec3c31c767020ba5c9991561edc943fb
msmith46234 <msmith46234@yahoo.com>
2015-09-29 17:20:59 -0400

    made it pretty

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Definition.create(word: Faker::Lorem.word, meaning: Faker::Lorem.paragraph)
